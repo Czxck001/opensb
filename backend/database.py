f817cab1e14587757130dfe74aa9cba23933d290
yaozerong <yaozerong@sensetime.com>
2017-06-01 19:54:24 +0800

    use async function to update mdb

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,23 @@
 # encoding: utf-8
+
+from threading import Thread
+from functools import wraps
+import sqlite3
+
+
+def async_func(func):
+    @wraps(func)
+    def asyncf(*args, **kwargs):
+        func_h = Thread(target = func, args = args, kwargs = kwargs)
+        func_h.start()
+        return func_h
+
+    return asyncf
+
 class MemoryDatabase:
     def __init__(self, db_path):
-        import sqlite3
         self._db = sqlite3.connect(db_path)
-
+        self.db_path = db_path
         # prepare database
         cu = self._db.cursor()
         cu.execute("""
@@ -35,21 +49,25 @@ class MemoryDatabase:
         cu.close()
         return ret
 
+    @async_func
     def update_memory(self, memory):
-        cu = self._db.cursor()
+        db = sqlite3.connect(self.db_path)
+        cu = db.cursor()
         cu.executemany(
             "REPLACE INTO proficiency(word, proficiency) VALUES(?, ?)",
             list(memory.items())
         )
-        self._db.commit()
+        db.commit()
         cu.close()
 
+    @async_func
     def log_word(self, word, known):
         import datetime
-        cu = self._db.cursor()
+        db = sqlite3.connect(self.db_path)
+        cu = db.cursor()
         cu.execute(
             "INSERT INTO log(time, word, known) VALUES(?, ?, ?)",
             (datetime.datetime.now(), word, known)
         )
-        self._db.commit()
+        db.commit()
         cu.close()
