c97d2dc7121641d98e57a62a42e10c81729197a8
yaozerong <zearom32@gmail.com>
2017-06-02 16:49:30 +0800

    use thread-safe queue and executor to update db

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,36 @@
 # encoding: utf-8
+
+import queue
+import sqlite3
+from functools import wraps
+from threading import Thread
+
+
+def async_func(func):
+    def worker():
+        while True:
+            task = async_func.queue.get()
+            task()
+
+    if not hasattr(async_func, 'queue'):
+        async_func.queue = queue.Queue()
+    if not hasattr(async_func, 'wt'):
+        async_func.wt = Thread(target=worker)
+        async_func.wt.setDaemon(True)
+        async_func.wt.start()
+
+    @wraps(func)
+    def asyncf(*args, **kwargs):
+        task = lambda: func(*args, **kwargs)
+        async_func.queue.put(task)
+
+    return asyncf
+
+
 class MemoryDatabase:
     def __init__(self, db_path):
-        import sqlite3
         self._db = sqlite3.connect(db_path)
-
+        self.db_path = db_path
         # prepare database
         cu = self._db.cursor()
         cu.execute("""
@@ -35,21 +62,25 @@ class MemoryDatabase:
         cu.close()
         return ret
 
+    @async_func
     def update_memory(self, memory):
-        cu = self._db.cursor()
+        db = sqlite3.connect(self.db_path)
+        cu = db.cursor()
         cu.executemany(
             "REPLACE INTO proficiency(word, proficiency) VALUES(?, ?)",
             list(memory.items())
         )
-        self._db.commit()
+        db.commit()
         cu.close()
 
+    @async_func
     def log_word(self, word, known):
         import datetime
-        cu = self._db.cursor()
+        db = sqlite3.connect(self.db_path)
+        cu = db.cursor()
         cu.execute(
             "INSERT INTO log(time, word, known) VALUES(?, ?, ?)",
             (datetime.datetime.now(), word, known)
         )
-        self._db.commit()
+        db.commit()
         cu.close()
